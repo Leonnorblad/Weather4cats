46ec445bb9fbf6255749b222aac67b354476ea9f
Leonard Norblad <norblad88@gmail.com>
2022-09-27 08:52:17 +0200

    disc

---
+++ R/Pack_disc.R
@@ -0,0 +1,10 @@
+#' Weather forecast API
+#' 
+#' Weather forecast data for five large cities in Sweden. Could be used with a shiny application.
+#' 
+#' @docType package
+#' 
+#' @author Leonard Persson Norblad \email{leope744@student.liu.se} Jacob Sibanda \email{jacsi259@student.liu.se}
+#' 
+#' @name Weather4cats
+NULL
